3eff96ed0fd2b2ecf0a402c095b34ae3137f2109
ASH <achraf9431@gmail.com>
2020-02-20 18:22:30 +0100

    add ability to get super classes tree

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-def get_subtree(cl, depth=0):
+def get_tree(cl, depth=0, kind='SUB'):
+    mapper = {
+        'SUB': '__subclasses__',
+        'SUPER': '__bases__',
+    }
+    
     if depth:
         print(' ' * 3 * depth, '|-->', sep='', end='')
+    
     print(f'{cl.__name__}')
+    res = getattr(cl, mapper[kind])
 
-    for c in cl.__subclasses__():
-        get_subtree(c, depth + 1)
+    for c in res() if callable(res) else res:
+        get_tree(c, depth=depth + 1, kind=kind)
